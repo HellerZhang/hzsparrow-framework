502329d40dd90ab694228734f6d5f2dde24edb82
HellerZhang <664053205@qq.com>
2020-08-13 17:18:28 +0800

    TASK 将base和generate项目从framework项目中移除  另外单独管理

--- hzsparrow-boot-base/src/main/java/com/hzsparrow/business/base/validgroups/Edit.java
+++
@@ -1,4 +0,0 @@
-package com.hzsparrow.business.base.validgroups;
-
-public interface Edit{
-}
--- hzsparrow-business-generate/src/main/java/com/hzsparrow/business/generate/mapper/MysqlGenMapper.java
+++
@@ -1,7 +0,0 @@
-package com.hzsparrow.business.generate.mapper;
-
-import org.springframework.stereotype.Component;
-
-@Component("mysqlGenMapper")
-public interface MysqlGenMapper extends GenMapper {
-}
